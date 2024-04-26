b8b87a79b70b1fa4015816c979e1dae7625c43ee
SimoonLight <forsimonmail@gmail.com>
2024-04-26 16:57:12 +0400

    Add project files.

---
+++ WPF_task_4/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_task_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cities_cmbox.SelectedIndex = 0;
+            List<string> azerbaijanCities = new List<string>
+            {
+                "Baku",
+                "Ganja",
+                "Sumqayit",
+                "Lankaran",
+                "Mingachevir",
+                "Shaki",
+                "Yevlakh",
+                "Gabala",
+                "Barda",
+                "Agdash",
+                "Sheki",
+                "Nakhchivan (City)",
+                "Khachmaz",
+                "Shamakhi",
+                "Quba"
+            };
+            cities_cmbox.ItemsSource = azerbaijanCities;
+
+            listbox1.ItemsSource = Database.people;
+
+        }
+
+        private void Save_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (ad_txtbox.Text != "" && soyad_txtbox.Text != "" && (male_rb.IsChecked.Value == true || female_rb.IsChecked.Value == true) && cities_cmbox.Text != "")
+            {
+                string s;
+                if (male_rb.IsChecked.Value == true)
+                {
+                    s = "Male";
+                }
+                else
+                {
+                    s = "Female";
+                }
+
+                bool c;
+                if (step_chbox.IsChecked.Value == true)
+                {
+                    c = true;
+                }
+                else
+                {
+                    c = false;
+                }
+
+                Person person = new Person(ad_txtbox.Text, soyad_txtbox.Text, s, c, cities_cmbox.Text);
+                Database.people.Add(person);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Database.people.Count > 0)
+            {
+                if (listbox1.SelectedIndex >= 0 && listbox1.SelectedIndex < Database.people.Count)
+                {
+
+                    Database.people.RemoveAt(listbox1.SelectedIndex);
+                    listbox1.ItemsSource = null;
+                    listbox1.ItemsSource = Database.people;
+                }
+            }
+
+        }
+    }
+}
---
+++ WPF_task_4/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_task_4
+{
+    public class Database
+    {
+        public static List<Person> people = new List<Person>();
+
+        //public static void Show_all_persons()
+        //{
+        //    for (int i = 0; i < people.Count; i++)
+        //    {
+        //        people.ToString();
+        //    }
+        //}
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public bool Step_student { get; set; }
+        public string City { get; set; }
+
+        public Person()
+        {
+            this.Name = string.Empty;
+            this.Surname = string.Empty;
+            this.Sex = string.Empty;
+            this.City = string.Empty;
+            this.Step_student = false;
+        }
+        public Person(string name, string surname, string sex, bool step_student, string seher)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Sex = sex;
+            this.Step_student = step_student;
+            this.City = seher;
+        }
+        public override string ToString()
+        {
+            string s;
+            if (this.Step_student==true)
+            {
+                s = "Student";
+            }
+            else
+            {
+                s = "Not Student";
+            }
+
+            return $"{this.Name}-{this.Surname}-{this.City}-{this.Sex}-{s}";
+        }
+    }
+}
